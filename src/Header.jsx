8846367d3d37781aa68535c413d836ca927d1da4
Bruno <bruno-nyland@hotmail.com>
2025-10-09 16:35:48 -0400

    Initial commit

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import chefIcon from "/chef3.svg" 
+
+function Header() {
+  return (
+    <header>
+      <img src={chefIcon} alt="Chefia Icon"/>
+      <h1>Chefia</h1>
+    </header>
+  )
+}
+
+export default Header

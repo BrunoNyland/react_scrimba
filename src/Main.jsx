4651e95a843227324ddc5b3add6dd77abc3ccf52
Bruno <bruno-nyland@hotmail.com>
2025-10-10 15:23:28 -0400

    Remove App component and add Main component with ingredient form

---
+++ src/Main.jsx
@@ -0,0 +1,12 @@
+function Main() {
+  return (
+    <section className="section-add-ingredient">
+      <form action="">
+        <input type="text" name="" id="" placeholder="Exemplo: Orégano" aria-label="Adicione um ingrediente"/>
+        <button>Adicione um ingrediente</button>
+      </form>
+    </section>
+  )
+}
+
+export default Main
